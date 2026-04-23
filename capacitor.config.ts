dee9b0325f85c39521d88b7883e7f048d195834d
israiali8851-arch <israiali8851@gmail.com>
2026-04-23 22:52:31 +0530

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.israiali88.sharepal',
+  appName: 'SharePal Pro',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
+
